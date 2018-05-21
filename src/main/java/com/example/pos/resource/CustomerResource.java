9d3ad8c53c2790522fffc425472548306914af99
honey2156@gmail.com <honey2156@gmail.com>
2018-05-22 02:31:52 +0530

    Added cross origin

--- src/main/java/com/example/pos/resource/CustomerResource.java
+++ src/main/java/com/example/pos/resource/CustomerResource.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import com.example.pos.service.CustomerService;
 
 @RestController
 @RequestMapping(value = "customers")
+@CrossOrigin(origins = { "http://localhost:4200" })
 public class CustomerResource {
 
 	@Autowired
@@ -23,11 +25,11 @@ public class CustomerResource {
 		return customerService.getAllCustomers();
 	}
 
-	@GetMapping(value="/seed")
+	@GetMapping(value = "/seed")
 	public void seedCustomers() {
 		List<Customer> seededCustomers = new ArrayList<>();
 		for (int i = 1; i < 10; i++) {
-			seededCustomers.add(new Customer("Customer" + i, "customer" + i + "@email.com", "123456789"+i));
+			seededCustomers.add(new Customer("Customer" + i, "customer" + i + "@email.com", "123456789" + i));
 		}
 		customerService.seedCustomers(seededCustomers);
 	}
--- src/main/java/com/example/pos/resource/ProductResource.java
+++ src/main/java/com/example/pos/resource/ProductResource.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import com.example.pos.service.ProductService;
 
 @RestController
 @RequestMapping(value = "/products")
+@CrossOrigin(origins = { "http://localhost:4200" })
 public class ProductResource {
 
 	@Autowired
